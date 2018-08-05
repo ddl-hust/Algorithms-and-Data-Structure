f820fad1aa54469e934f9593db4ce0dc39cceb3d
wustdeng <ddl196526@163.com>
2018-08-05 15:55:59 +0800

    a gengeral type

--- 括号检查.py
+++ 括号检查.py
@@ -1,22 +1,28 @@
 from Stack import *
 def parChecker(string):
-	"""使用堆栈实现括号完整检查
+	"""using stack to implent check
 	"""
 	s=Stack()
 	balanced= True
 	index=0
 	while index<len(string) and balanced:
 		symbol=string[index]
-		if symbol=="(":
+		if symbol in "([{":
 			s.push(symbol)
 		else:
 			if s.isEmpty():
 				balanced=False
 			else:
-				s.pop()
+				top=s.pop()
+				if not matches(top,symbol):
+					balanced=False
 		index+=1
 	if balanced and s.isEmpty():
 		return True
 	else:
 		return False
-print(parChecker('((()))'))
+def matches(open,close):
+	opens="[{("
+	closes="]})"
+	return opens.index(open)==closes.index(close)
+print(parChecker('[{()}]'))
