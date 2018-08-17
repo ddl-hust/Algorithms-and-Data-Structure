2efbb777bc39d072493695b406a9f3ed74c7c3ae
wustdeng <ddl196526@163.com>
2018-08-17 15:37:14 +0800

    插入排序

---
+++ 插入排序.py
@@ -0,0 +1,9 @@
+#O(n^2) 
+def insertionSort(alist):
+	for index in range(1,len(alist)):
+		currentvalue=alist[index]
+		position=index
+		while position>0 and alist[position-1]>currentvalue:
+			alist[position]=alist[position-1] #移位操作
+			position=position-1
+		alist[position]=currentvalue
